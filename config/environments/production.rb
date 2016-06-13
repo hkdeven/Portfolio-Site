5afbf4a00aad0b9c9f877ddc2b4a444b8f91a7b2
Deven Blackburn <deven@codefll.org>
2016-06-13 10:12:29 -0400

    Heroku push testing cdn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,8 +12,8 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
-
+  # config.action_controller.perform_caching = true
+  config.action_controller.asset_host = 'deven-444e.kxcdn.com'
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
