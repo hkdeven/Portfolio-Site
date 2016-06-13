9175c5bd38b810e4c40fef9c09ed1cdde9b4405d
Deven Blackburn <deven@codefll.org>
2016-06-13 10:32:43 -0400

    Heroku push testing cdn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,9 +2,9 @@ Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
   config.consider_all_requests_local       = false
-  # config.action_controller.perform_caching = true
-  config.action_controller.asset_host = 'deven-444e.kxcdn.com'
-  # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.action_controller.perform_caching = true
+  # config.action_controller.asset_host = 'deven-444e.kxcdn.com'
+  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.js_compressor = :uglifier
   config.assets.compile = false
   config.assets.precompile += %w(.svg .eot .woff .ttf)
