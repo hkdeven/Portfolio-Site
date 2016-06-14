b5df8e5be113bfa6a387445f71a4ea8cf8f7ae66
Deven Blackburn <deven@codefll.org>
2016-06-13 22:04:01 -0400

    Border removed completely. Projects view started.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def resume
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :articles
-
+  resources :projects, controller: 'articles'
+  get 'resume', to: 'welcome#show'
   root 'welcome#index'
 end
