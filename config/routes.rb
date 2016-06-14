8fdd7e39b52a47156b8632f12d79605c106dd7d4
Deven Blackburn <deven@codefll.org>
2016-06-14 12:59:23 -0400

    Icons swapped and updated.  Cleanup of unused assets.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+
 Rails.application.routes.draw do
   root 'welcome#index'
   resources :articles
   resources :projects, controller: 'articles'
-  get 'resume', to: 'welcome#show'
+  # get 'projects', to: 'articles#index'
+  get 'contact', to: 'welcome#show'
 end
