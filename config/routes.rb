9ff839c87dda8aabcd4c4f1b1f5002c308bca483
Deven Blackburn <deven@codefll.org>
2016-06-18 14:38:23 -0400

    Resume view path clarified, added to routes, linked in Navbar. Resume pdf updated to match new color scheme.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   resources :articles
-  # resources :projects, controller: 'articles'
   get 'projects', to: 'articles#index'
   get 'contact', to: 'welcome#contact'
   get 'github', to: 'articles#github'
   get 'blog', to: 'articles#blog'
+  get 'resume', to: 'articles#resume'
 end
