c1caec2211d9d022b95060820b6483c5e5f6a904
hkdeven <deven@codefll.org>
2016-12-11 15:33:37 -0500

    Blog featured released.:happy:

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  resources :articles
   get 'projects', to: 'pages#projects'
   get 'contact', to: 'pages#contact'
   get 'github', to: 'pages#github'
   get 'blog', to: 'pages#blog'
   get 'resume', to: 'pages#resume'
   get 'demos', to: 'pages#demos'
+  resources :articles do
+    resources :commments
+  end
 end
