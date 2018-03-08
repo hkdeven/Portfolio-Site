72873c131805d977a3c054e77686f41f136c54c1
hkdeven <deven@makecodenotwar.co>
2018-03-08 11:12:46 -0500

    Routes updated for GA verification :+1:

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'resume', to: 'pages#resume'
   get 'demos', to: 'pages#demos'
   get 'lp', to: 'pages#lp'
+  get 'google2ac5bbddc416ebc3', to: 'pages#google2ac5bbddc416ebc3.html'
   resources :articles do
     resources :commments
   end
